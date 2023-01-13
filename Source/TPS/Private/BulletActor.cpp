7d5a6075818fde226102f5592788472e9821ff99
jidoil <57009810+yoponchik@users.noreply.github.com>
2023-01-13 15:14:42 +0900

    Create BulletActor Script

---
+++ Source/TPS/Private/BulletActor.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BulletActor.h"
+#include <Components/SphereComponent.h>
+#include <Components/StaticMeshComponent.h>
+#include <GameFramework/ProjectileMovementComponent.h>
+
+
+
+// Sets default values
+ABulletActor::ABulletActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	#pragma region Components
+	sphereComp = CreateDefaultSubobject<USphereComponent>("Collider Component");
+	SetRootComponent(sphereComp);
+	sphereComp->SetSphereRadius(13);
+	sphereComp->SetCollisionProfileName(TEXT("BlockAll"));
+
+	meshComp= CreateDefaultSubobject<UStaticMeshComponent>("Mesh Component");
+	meshComp->SetupAttachment(RootComponent);
+	meshComp->SetRelativeScale3D(FVector(0.25f));
+	meshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);					//can do presets
+	//meshComp->SetCollisionProfileName(TEXT("NoCollision"));
+
+	projMoveComp = CreateDefaultSubobject<UProjectileMovementComponent>("Projectile Component");
+	projMoveComp->SetUpdatedComponent(sphereComp);
+	projMoveComp->InitialSpeed = 5000;
+	projMoveComp->MaxSpeed = 5000;
+	projMoveComp->bShouldBounce = true;						//note that this variable is b because its a bool
+	projMoveComp->Bounciness = 0.5f;
+	
+	#pragma endregion
+
+}
+
+// Called when the game starts or when spawned
+void ABulletActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABulletActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TPS/Public/BulletActor.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BulletActor.generated.h"
+
+UCLASS()
+class TPS_API ABulletActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABulletActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+#pragma region Components
+	UPROPERTY(EditAnywhere, Category = "Bullet Settings | Components")
+	class USphereComponent* sphereComp;
+
+	UPROPERTY(EditAnywhere, Category = "Bullet Settings | Components")
+	class UStaticMeshComponent* meshComp;
+	
+	UPROPERTY(EditAnywhere, Category = "Bullet Settings | Components")
+	class UProjectileMovementComponent* projMoveComp;
+#pragma endregion
+
+
+};
