edfd0e082f1a7bdf5a3da40fb9388ee71f02241f
yoponchik <57009810+yoponchik@users.noreply.github.com>
2023-02-02 12:31:57 +0900

    Update TPS.Build.cs
    -Add Navigation System Module

--- Source/TPS/TPS.Build.cs
+++ Source/TPS/TPS.Build.cs
@@ -8,7 +8,7 @@ public class TPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" , "AIModule"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" , "AIModule", "NavigationSystem"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
